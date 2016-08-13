6445ee654a8ec10982b48bea1dfe0266b5bf9d16
Danimar Ribeiro <danimaribeiro@gmail.com>
2016-08-13 08:40:37 -0300

    [WIP] Classe para gerar boleto cecred

--- pyboleto/bank/cecred.py
+++ pyboleto/bank/cecred.py
@@ -10,7 +10,7 @@ class BoletoCecred(BoletoData):
 
     agencia_cedente = CustomProperty('agencia_cedente', 4)
     conta_cedente = CustomProperty('conta_cedente', 6)
-    codigo_beneficiario = CustomProperty('codigo_beneficiario', 7)
+    codigo_beneficiario = CustomProperty('codigo_beneficiario', 6)
     nosso_numero = CustomProperty('nosso_numero', 7)
 
     carteira = CustomProperty('carteira', 1)
@@ -18,9 +18,22 @@ class BoletoCecred(BoletoData):
     def __init__(self):
         super(BoletoCecred, self).__init__()
 
-        self.codigo_banco = "756"
+        self.codigo_banco = "085"
         self.logo_image = "logo_cecred.jpg"
         self.especie_documento = 'DM'
-        self.label_cedente = 'Coop Contr/Cód Beneficiário'
-        self.local_pagamento = 'Pagável Preferencialmente nas Cooperativas ' +\
-            'da Rede Sicoob ou Qualquer Banco até o Vencimento.'
+        self.label_cedente = u'Agência/Código Beneficiário'
+        self.local_pagamento = u'Pagável Preferencialmente nas Cooperativas '\
+            u'do sistema Cecred. Após venc. somente na cooperativa'
+
+    @property
+    def codigo_dv_banco(self):
+        return self.codigo_banco + '-1'
+
+    @property
+    def campo_livre(self):
+        content = "%2s%5s%6s%8s%4s" % (self.carteira.zfill(2),
+                                       self.agencia_cedente.zfill(5),
+                                       self.codigo_beneficiario.zfill(6),
+                                       self.nosso_numero.zfill(8),
+                                       '0851')
+        return content
