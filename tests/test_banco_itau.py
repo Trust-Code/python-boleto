be1b7659135bfd8341c201a90275d6a479e37149
Felipe Paloschi <paloschi.eca@gmail.com>
2018-08-13 11:13:17 -0300

    Fix tests itau

--- tests/test_banco_itau.py
+++ tests/test_banco_itau.py
@@ -15,6 +15,7 @@ class TestBancoItau(BoletoTestCase):
             d.carteira = '109'
             d.agencia_cedente = '0293'
             d.conta_cedente = '01328'
+            d.conta_cedente_dv = '1'
             d.data_vencimento = datetime.date(2009, 10, 19)
             d.data_documento = datetime.date(2009, 10, 19)
             d.data_processamento = datetime.date(2009, 10, 19)
@@ -26,13 +27,13 @@ class TestBancoItau(BoletoTestCase):
     def test_linha_digitavel(self):
         self.assertEqual(
             self.dados[0].linha_digitavel,
-            '34191.09008 00015.710296 30132.800001 9 43950000002980'
+            '34191.09008 00015.710296 30132.810000 4 43950000002980'
         )
 
     def test_codigo_de_barras(self):
         self.assertEqual(
             self.dados[0].barcode,
-            '34199439500000029801090000015710293013280000'
+            '34194439500000029801090000015710293013281000'
         )
 
     def test_agencia(self):
